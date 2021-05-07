843444acac3e20f413e012d2e513d96d3e6c899f
RndmCodeGuy20 <shantanu.mane.200@outlook.com>
2021-05-07 16:53:58 +0530

    master

--- C.P. Practicals/readfile.c
+++ C.P. Practicals/readfile.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <conio.h>
+#include <stdlib.h>
 
 int main()
 {
-    
+    FILE *fptr;
+
+    fptr = fopen("Stud_info_file.txt", "r");
+
+    if (fptr == NULL)
+    {
+        printf("Error opening the file!!!");
+        return 1;
+    }
+
+    printf("\nStudents Information --> \t\n");
+
+    char c = fgetc(fptr);
+
+    while (c != EOF)
+    {
+        putchar(c);
+        c = getc(fptr);
+    }
+    fclose(fptr);
 
     return 0;
 }

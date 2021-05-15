ab7b0d90c54f4707a11f7f2f354dc07f682bfdc7
Shantanu Mane <shantanu.mane.200@outlook.com>
2021-05-15 12:30:35 +0530

    master change on preprocessor test file

--- test.c
+++ test.c
@@ -6,7 +6,7 @@ int main()
     printf("%s\n", __FILE__);
     printf("%s\n", __DATE__);
     printf("%s\n", __TIME__);
-    printf("%s\n", __STDC__);
+    printf("%d\n", __LINE__);
 
     return 0;
 }

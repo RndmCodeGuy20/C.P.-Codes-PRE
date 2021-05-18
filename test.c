8787f9488da932d88c1fb4c1d73f10e8c20bcc87
Shantanu Mane <shantanu.mane.200@outlook.com>
2021-05-18 08:49:06 +0530

    documentation on quadfunc.c

--- test.c
+++ test.c
@@ -1,3 +1,13 @@
+/**
+ * @file quadfunc.c
+ * @author Shantanu Mane (shantanu.mane.200@outlook.com)
+ * @brief 
+ * @version  420.69
+ * @date 2021-05-18
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>

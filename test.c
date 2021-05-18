aaf9a9a1e96f86cb816ba9c4410f19118334e57c
Shantanu Mane <shantanu.mane.200@outlook.com>
2021-05-18 08:47:27 +0530

    test func to quadratic func master build

--- test.c
+++ test.c
@@ -1,43 +1,42 @@
-/**
- * @file test.c
- * @author Shantanu Mane (shantanu.mane.200@outlook.com)
- * @brief 
- * @version  420.69
- * @date 2021-05-17
- * 
- * @copyright Copyright (c) 2021
- * 
- */
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
 
-int n;
-
-int m = 10;
-
-int sum(int *x, int *y)
-{
-
-    int sum;
-
-    sum = *x + *y;
-    printf("Output from function : %d and m = %d\n", sum, m * 0);
-    return sum;
-}
+float deter(float x, float y, float z);
+float sol(float m, float n, float o);
 
 int main()
 {
-    static int z;
-    int a, b;
 
-    scanf("%d %d", &a, &b);
+    float a, b, c;
 
-    z = sum(&a, &b);
-    char ch = {104};
-    printf("%d %c %d", z, ch, n);
+    printf("Enter the co-efficient of the given quadratic : ");
+
+    scanf("%f %f %f", &a, &b, &c);
+
+    double d = deter(a, b, c);
+
+    printf("\nDeterminant of the given equation is : %.3lf\n", d);
+
+    sol(a, b, d);
 
     return 0;
+}
+float deter(float x, float y, float z)
+{
+    double d;
+    d = sqrt((y * y) - (4 * x * z));
+
+    return d;
+}
+float sol(float m, float n, float o)
+{
+    float sol1, sol2;
+
+    sol1 = ((-n) + o) / (2 * m);
+    sol2 = ((-n) - o) / (2 * m);
+
+    printf("\nThe solution of the given quadratic expression is : %.3f %.3f", sol1, sol2);
 }

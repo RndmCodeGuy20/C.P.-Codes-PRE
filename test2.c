4836ad406d0dd25ce4a7ba8b6ccb079243701a84
Shantanu Mane <shantanu.mane.200@outlook.com>
2021-05-22 07:25:50 +0530

    commit on web dev

---
+++ test2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int n = 2, i,m;
+
+    float sum;
+
+    printf("Enter the limit : ");
+    scanf("%d", &m);
+
+    return 0;
+}

6eb78bb612efa41055b1950e043b52aafbd7ad19
Shantanu Mane <shantanu.mane.200@outlook.com>
2021-05-26 16:57:22 +0530

    pyrnum spacing change and tested

--- C Aptitude Questions/pyrnum.c
+++ C Aptitude Questions/pyrnum.c
@@ -1,26 +1,29 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
 
 int main()
 {
-int i,j,rows,spc,k,t=1;
+    int i, j, rows, spc, k, t = 1;
 
-printf("Enter the number of rows needed: ");
-scanf("%d",&rows);
+    printf("Enter the number of rows needed: ");
+    scanf("%d", &rows);
 
-spc=rows;
+    spc = rows;
 
-for(i=1;i<=rows;i++){
+    for (i = 1; i <= rows; i++)
+    {
 
-    for(k=spc;k>=1;k--){
-        printf(" ");
+        for (k = spc; k >= 1; k--)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d ", t++);
+        }
+        spc--;
+        printf("\n");
     }
-    for(j=1;j<=i;j++){
-        printf("%d ",t++);
-    }
-    spc--;
-    printf("\n");
-} 
 
-return 0;
+    return 0;
 }

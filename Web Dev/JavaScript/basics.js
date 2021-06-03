c89fcb4004dffbf1ac7c9889e58ad4017e90b186
Shantanu Mane <shantanu.mane.200@outlook.com>
2021-06-03 08:22:22 +0530

    javascrip file optimised

--- Web Dev/JavaScript/basics.js
+++ Web Dev/JavaScript/basics.js
@@ -2,13 +2,11 @@ console.log("Hello World");
 
 const x = 30;
 
-console.log(x%9);
-
+console.log(x % 9);
 
 let y = 10;
 
 var z = x + y;
 if (z >= 10) {
-    
-
-console.log(z);}
+  console.log(z);
+}

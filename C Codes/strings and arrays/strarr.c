4ac147ea20dd3140509dcc7830aefb8d4cbf26bd
Shantanu Mane <shantanu.mane.200@outlook.com>
2021-06-06 14:42:09 +0530

    added %s in code and tried : master

--- C Codes/strings and arrays/strarr.c
+++ C Codes/strings and arrays/strarr.c
@@ -5,11 +5,13 @@ int main()
 {
 int i=0;
 char name[]="Hello World!";
-
+//*Using while method.
 while (name[i]!='\0')
 {
     printf("%c ",name[i]);
     i++;
 }
+//*Using string output method.
+printf("%s", name);
 return 0;
 }
